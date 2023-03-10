99627a0512e1def55d54f26d8f2a93e2bf4dbbd1
SBTR <03sh.bel.tr@gmail.com>
2023-03-10 10:29:21 -0600

    Se implementó Lombok.

--- ExamenSemana3/sprinMvcHibernateJdbcSBoot/src/main/java/com/luv2code/springdemo/entity/Libro.java
+++ ExamenSemana3/sprinMvcHibernateJdbcSBoot/src/main/java/com/luv2code/springdemo/entity/Libro.java
@@ -7,6 +7,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name="libro")
 public class Libro {
@@ -29,72 +36,9 @@ public class Libro {
 	@Column(name="disponible")
 	private boolean disponible;
 	
-	public Libro() {
-		}//constructor vacío
-
-	public Libro(int id, String titulo, String autor, String editorial, boolean disponible) {
-		this.id = id;
-		this.titulo = titulo;
-		this.autor = autor;
-		this.editorial = editorial;
-		this.disponible = disponible;
-	}
-
-	public Libro(String titulo, String autor, String editorial, boolean disponible) {
-		this.titulo = titulo;
-		this.autor = autor;
-		this.editorial = editorial;
-		this.disponible = disponible;
-	}
+	//LOMBOK GENERA: setters, getters, constructor vacío, constructor con todos los param y con los requeridos
 
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getAutor() {
-		return autor;
-	}
-
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-
-	public String getEditorial() {
-		return editorial;
-	}
-
-	public void setEditorial(String editorial) {
-		this.editorial = editorial;
-	}
-
-	//is y set de nueva columna
-	public boolean isDisponible() {
-		return disponible;
-	}
-
-	public void setDisponible(boolean disponible) {
-		this.disponible = disponible;
-	}
-
-	//nuevo toString
-	@Override
-	public String toString() {
-		return "Libro [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", editorial=" + editorial
-				+ ", disponible=" + disponible + "]";
-	}
 
 }
 
